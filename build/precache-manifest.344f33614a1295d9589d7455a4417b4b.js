4350e8f8b9956df2b6710acf5944c652af5ecf63
lukechang24 <lukechang244@gmail.com>
2020-05-08 22:18:26 -0700

    added firebase hosting

---
+++ build/precache-manifest.344f33614a1295d9589d7455a4417b4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b820b5a12ddacd144a6acbeecebf740",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e951579c66634560eba5",
+    "url": "/static/css/main.30fc6684.chunk.css"
+  },
+  {
+    "revision": "fc2efaa6a7fcf6c838fc",
+    "url": "/static/js/2.cc4a8c50.chunk.js"
+  },
+  {
+    "revision": "73138c634db8d37252cbabcb33689813",
+    "url": "/static/js/2.cc4a8c50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e951579c66634560eba5",
+    "url": "/static/js/main.77530eb0.chunk.js"
+  },
+  {
+    "revision": "9a5aae673eef5c7d6bd7",
+    "url": "/static/js/runtime-main.77147d78.js"
+  }
+]);
